68c257a521816534c0e9464044b69db48f97d863
mkuwan <mkuwan@msn.com>
2023-10-16 17:16:30 +0900

    Sortを追加

---
+++ src/main/java/com/example/functional/declarative/Sorting.java
@@ -0,0 +1,61 @@
+package com.example.functional.declarative;
+
+import com.example.functional.mockdata.MockData;
+import com.example.functional.model.Car;
+import com.example.functional.model.Person;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Sorting {
+
+    @Test
+    public void sortingSteamOfElements() throws IOException {
+        List<Person> people = MockData.getPeople();
+        List<String> sorted = people.stream()
+                .map(Person::getFirstName)
+                .sorted()
+                .collect(Collectors.toList());
+        sorted.forEach(System.out::println);
+    }
+
+    @Test
+    public void sortingSteamOfElementsReverse() throws IOException {
+        List<Person> people = MockData.getPeople();
+
+        List<String> sorted = people.stream()
+                .map(Person::getFirstName)
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+        sorted.forEach(System.out::println);
+    }
+
+    @Test
+    public void sortingSteamOfObjets() throws IOException {
+        List<Person> people = MockData.getPeople();
+
+        Comparator<Person> comparing = Comparator
+                .comparing(Person::getEmail)
+                .reversed()
+                .thenComparing(Person::getFirstName);
+
+        List<Person> sort = people.stream()
+                .sorted(comparing)
+                .collect(Collectors.toList());
+        sort.forEach(System.out::println);
+    }
+
+    @Test
+    public void topTenMostExpensiveBlueCars() throws IOException {
+        List<Car> cars = MockData.getCars();
+        List<Car> topTen = cars.stream()
+                .filter(car -> car.getColor().equalsIgnoreCase("bLuE"))
+                .sorted(Comparator.comparing(Car::getPrice).reversed())
+                .limit(10)
+                .collect(Collectors.toList());
+        topTen.forEach(System.out::println);
+    }
+}
